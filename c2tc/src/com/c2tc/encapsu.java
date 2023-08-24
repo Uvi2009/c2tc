15774f35f59d2b30e2f64ebce1137bf6c4e39316
yuvar <yuvar@DESKTOP-270VS22>
2023-08-24 10:02:22 +0530

    hkjn

---
+++ c2tc/src/com/c2tc/encapsu.java
@@ -0,0 +1,10 @@
+package com.c2tc;
+import java.io.*;
+public class Encapsu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hlo");
+	}
+
+}
